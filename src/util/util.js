da05df465ec717e2e520926fa231a868aba34616
chris <tigerman33@gmx.net>
2022-01-29 21:05:42 +0100

    Added clone to utils

--- src/util/util.js
+++ src/util/util.js
@@ -1,5 +1,7 @@
 module.exports = {
 	arrayIntersect: (arr1, arr2) => arr1.filter(x => arr2.includes(x)),
 
-	arrayDifference: (arr1, arr2) => arr1.filter(x => !arr2.includes(x))
+	arrayDifference: (arr1, arr2) => arr1.filter(x => !arr2.includes(x)),
+
+	clone: require('just-clone')
 }
