fdd509dc7b2af9cd99e3a9bcfc97d898f8facadd
chris <tigerman33@gmx.net>
2022-02-04 15:49:45 +0100

    Quick-n-dirty cli

--- quartersbrief.js
+++ quartersbrief.js
@@ -1,4 +1,5 @@
 import { init } from './quartersbrief.conf.js';
+import log from 'loglevel';
 import { readFileSync } from 'fs';
 import { gameObjectFactory } from './src/model/gameobjectfactory.js';
 import { Ship } from './src/model/ship.js';
@@ -10,23 +11,41 @@ import node_readline from 'readline';
 
 
 
-var data = JSON.parse(readFileSync('data/GameParams.json'));
-assertInvariants(data);
+let data = JSON.parse(readFileSync('data/GameParams.json'));
+try {
+	assertInvariants(data);
+} catch (x) {
+	log.error(x.errors);
+	process.exit(1);
+}
 
 gameObjectFactory.setEverything(data);
 
 const readline = node_readline.createInterface({ input: process.stdin, output: process.stdout, prompt: 'Quarters Brief>' });
 readline.prompt();
+let ship;
 readline.on('line', function(input) {
-	if (input.startsWith('stats ')) {
-		input = input.substring(6);
-		let ship = gameObjectFactory.createGameObject(input);
-		ship.applyConfiguration('stock');
-		console.log(`${ship.getName()}'s stats are: \n  
-			rudder shift time: ${ship.getCurrentConfiguration().getRuddershift()}\n
-			health: ${ship.getCurrentConfiguration().getHealth()}\n
-			turning circle: ${ship.getCurrentConfiguration().getTurningCircle()}`);
+	let command = input.split(' ')[0].trim().toLowerCase();
+	let param = input.split(' ').splice(1).join(' '); 
+
+	switch (command) {
+		case 'list':
+			console.log(`Codes for ${param}: \n ${gameObjectFactory.listCodesForType(param)}`);
+			break;
+		case 'ship': 
+			ship = gameObjectFactory.createGameObject(param);
+			console.log(`Ship set to stock configuration of ${ship.name}`);
+			break;
+		case 'equip':
+			ship.equipModules(param);
+			console.log(`Ship ${ship.name} set to configuration ${param}`);
+			break;
+		case 'stat': 
+			console.log(`Value is ${ship['get' + param].call(ship)}`);
+			break;
+		case 'exit':
+			readline.close();
+			process.exit(0);
 	}
-	if (input === 'exit') readline.close();
 	readline.prompt();
 });
