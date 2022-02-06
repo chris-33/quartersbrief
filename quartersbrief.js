3ceda221484757ccdef83899a798dd60edba136c
chris <tigerman33@gmx.net>
2022-02-06 08:20:03 +0100

    Renamed some commands and added help text

--- quartersbrief.js
+++ quartersbrief.js
@@ -9,7 +9,12 @@ import process from 'process';
 import node_readline from 'readline';
 
 
-
+const HELPTEXT = 'Known commands:\n' +
+'		list <type>\t\t\tPrint all known reference codes that have that type\n' +
+'		ship <ship>\t\t\tSet the current ship to <ship>. <ship> can be a numeric ID, a reference code, or a reference name' + 
+'		modules <descriptor>\t\tSet the current ship\'s module configuration to <descriptor>\n' +
+'		upgrade <upgrade>\t\tEquip an upgrade on the current ship. <upgrade> can be a numeric ID, a reference code, or a reference name\n' +
+'		stat <property>\t\t\tPrint the value for the current ship\'s property, considering all equipped modules and upgrades';
 
 let data = JSON.parse(readFileSync('data/GameParams.json'));
 try {
@@ -36,16 +41,27 @@ readline.on('line', function(input) {
 			ship = gameObjectFactory.createGameObject(param);
 			console.log(`Ship set to stock configuration of ${ship.name}`);
 			break;
-		case 'equip':
+		case 'modules':
 			ship.equipModules(param);
 			console.log(`Ship ${ship.name} set to configuration ${param}`);
+			break;
+		case 'upgrade':
+			let upgrade = gameObjectFactory.createGameObject(param);
+			if (ship.equipModernization(upgrade))
+				console.log(`Equipped upgrade ${upgrade.getName()} on ${ship.name}`);
+			else console.log(`Ship ${ship.name} is not eligible for upgrade ${upgrade.getName()}`)
 			break;
 		case 'stat': 
 			console.log(`Value is ${ship['get' + param].call(ship)}`);
 			break;
+		case 'help':
+			console.log(HELPTEXT);
+			break;
 		case 'exit':
 			readline.close();
 			process.exit(0);
+		default: 
+			console.log(`Unknown command ${command}. Type help to see a list of possible commands.`);
 	}
 	readline.prompt();
 });
