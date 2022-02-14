72f0725055affd37cbac98438ca689677e95447c
chris <tigerman33@gmx.net>
2022-02-14 09:14:02 +0100

    Added test dependencies mockfs, chai-as-promised

--- test/mocha.conf.js
+++ test/mocha.conf.js
@@ -2,9 +2,11 @@ import chai from 'chai';
 import sinon from 'sinon';
 import sinontest from 'sinon-test';
 import sinonchai from 'sinon-chai';
+import chaiaspromised from 'chai-as-promised';
 
 sinon.test = sinontest(sinon);
 
 chai.use(sinonchai);
+chai.use(chaiaspromised);
 
 global.expect = chai.expect;
