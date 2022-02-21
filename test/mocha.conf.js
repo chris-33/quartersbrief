979996e70015b70cff0b1844c1702a214db0b9ec
chris <tigerman33@gmx.net>
2022-02-21 21:08:49 +0100

    Silence logger for test runs

--- test/mocha.conf.js
+++ test/mocha.conf.js
@@ -4,6 +4,7 @@ import sinontest from 'sinon-test';
 import sinonchai from 'sinon-chai';
 import chaiaspromised from 'chai-as-promised';
 import chaievents from 'chai-events';
+import log from 'loglevel';
 
 sinon.test = sinontest(sinon);
 
@@ -12,3 +13,6 @@ chai.use(chaiaspromised);
 chai.use(chaievents);
 
 global.expect = chai.expect;
+
+// Prevent log output from polluting test report
+log.disableAll();
