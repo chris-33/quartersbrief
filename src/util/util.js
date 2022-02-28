ff4c8cc8191b522e28501198ca12c1dba41442e3
chris <tigerman33@gmx.net>
2022-02-28 20:32:33 +0100

    Added arrayEqual

--- src/util/util.js
+++ src/util/util.js
@@ -2,4 +2,6 @@ let arrayIntersect = (arr1, arr2) => arr1.filter(x => arr2.includes(x));
 
 let	arrayDifference = (arr1, arr2) => arr1.filter(x => !arr2.includes(x));
 
-export { arrayIntersect, arrayDifference }
+let arrayEqual = (arr1, arr2) => arr1.every(x => arr2.includes(x)) && arr2.every(x => arr1.includes(x));
+
+export { arrayIntersect, arrayDifference, arrayEqual }
