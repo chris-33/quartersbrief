7f617ff9a84cf9e42250fd89697383880fa03b05
chris <tigerman33@gmx.net>
2022-03-13 11:42:03 +0100

    Now using nock to emulate responses instead of mocking fetch()

--- test/util/wgapi.spec.js
+++ test/util/wgapi.spec.js
@@ -1,19 +1,25 @@
+import nock from 'nock';
 import { WargamingAPI } from '../../src/util/wgapi.js';
-import { Response } from 'node-fetch';
 import httpError from 'http-errors';
 const HTTPError = httpError.HttpError;
-import sinon from 'sinon';
 
-describe.only('WargamingAPI', function() {
+describe('WargamingAPI', function() {
 	let api;
+	let apiSrv;
 
-	beforeEach(function() {
-		sinon.stub(global, 'fetch');
-		api = new WargamingAPI('', '');
+	before(function() {
+		apiSrv = nock(/worldofwarships/);
+		nock.disableNetConnect();
 	});
 
-	afterEach(function() {
-		global.fetch.restore();
+	beforeEach(function() {
+		api = new WargamingAPI('', '');
+		nock.cleanAll();
+	});
+
+	after(function() {
+		nock.restore();
+		nock.enableNetConnect();
 	});
 
 	describe('WargamingAPI.APIError', function() {
@@ -44,47 +50,44 @@ describe.only('WargamingAPI', function() {
 
 	describe('.access', function() {
 		it('should throw an APIError if there is no application ID', async function() {
-			global.fetch.resolves(new Response(JSON.stringify({
+			apiSrv.get(/./).reply(200, {
 				status: 'error',
-				error: {
-					code: 407,
-					message: 'INVALID_APPLICATION_ID'
-				}				
-			})));
+				error: { code: 407, message: 'INVALID_APPLICATION_ID' }
+			});
 			return expect(api.access('', {})).to.be.rejectedWith(WargamingAPI.APIError);
 		});
 
 		it('should throw an APIError if the request limits have been exceeded', function() {
-			global.fetch.resolves(new Response(JSON.stringify({
+			apiSrv.get(/./).reply(200, {
 				status: 'error',
 				error: {
 					code: 407,
 					message: 'REQUEST_LIMIT_EXCEEDED'
 				}				
-			})));
+			});
 			return expect(api.access('', {})).to.be.rejectedWith(WargamingAPI.APIError);			
 		});
 
 		it('should throw an Error if there was an HTTP error', function() {
 			// Fetch returned 404 Not found
 			// (As per the spec, the promise still resolves)
-			global.fetch.resolves(new Response(null, { status: 404, statusCode: 'Not found' }));
+			apiSrv.get(/./).reply(404, 'Not found');//global.fetch.resolves(new Response(null, { status: 404, statusCode: 'Not found' }));
 			return expect(api.access('', {})).to.be.rejectedWith(HTTPError);
 		});
 
 		it('should throw an Error if there was a network error', function() {
 			// The network was unavailable, as per the spec, this is the only case where fetch rejects
-			global.fetch.rejects(Response.error());
+			// global.fetch.rejects(Response.error());
 			return expect(api.access('', {})).to.be.rejected;
 		});
 
 		it('should return the data field of a successful API access', function() {
 			const data = { prop: 'prop' };
-			global.fetch.resolves(new Response(JSON.stringify({
+			apiSrv.get(/./).reply(200, {
 				status: 'ok',
 				meta: { count: 1, hidden: null },
 				data: data
-			})));
+			});
 			return expect(api.access('', {})).to.eventually.deep.equal(data);
 		});
 	});
