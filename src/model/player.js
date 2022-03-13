707e240ff56667b66e6bd023c9e9e545fcd41b24
chris <tigerman33@gmx.net>
2022-03-13 18:20:40 +0100

    Added handling of hidden profiles

--- src/model/player.js
+++ src/model/player.js
@@ -7,12 +7,15 @@ class Player {
 		this._data = ComplexDataObject(data);
 	}
 
+	get hidden() { return this._data.hidden_profile; }
+
 	get name() { return this._data.nickname; }
-	get battles() { return this.bot ? NaN : this._data.statistics.pvp.battles; }
-	get victories() { return this.bot ? NaN : this._data.statistics.pvp.wins; }
+	get battles() { return this.bot ? NaN : this._data.statistics?.pvp?.battles; }
+	get victories() { return this.bot ? NaN : this._data.statistics?.pvp?.wins; }
 	get winrate() { 
+		if (this.hidden) return undefined; // Because undefined/undefined === NaN
 		// Treat someone who hasn't played any battles as 0% winrate
-		return this.battles === 0 ? 0 : this.victories / this.battles; 
+		return this.battles === 0 ? 0 : this.victories / this.battles;
 	}
 
 	static createBot(name) {
--- test/model/player.spec.js
+++ test/model/player.spec.js
@@ -1,12 +1,20 @@
 import { Player } from '../../src/model/player.js';
 
 describe('Player', function() {
+	const statistics = [ 'battles', 'victories', 'winrate' ];
+
 	describe('.winrate', function() {
 		it('should be 0 if the player has not fought any battles and the ratio of wins to fought battles otherwise', function() {
 			expect(new Player({ statistics: { pvp: { wins: 0, battles: 0 }}}).winrate).to.equal(0);
 			for (let i = 1; i < 6; i++) {
 				expect(new Player({ statistics: { pvp: { wins: 1, battles: i }}}).winrate).to.equal(1 / i);
 			}
+		});
+
+		it('should return undefined for all statistics for a hidden profile', function() {
+			let player = new Player({ hidden_profile: true });
+			for (let prop of statistics)
+				expect(player[prop]).to.be.undefined;
 		});
 	});
 
@@ -26,7 +34,7 @@ describe('Player', function() {
 		});
 
 		it('should return NaN for all statistics', function() {
-			for (let prop of [ 'battles', 'victories', 'winrate' ])
+			for (let prop of statistics)
 				expect(bot[prop], prop).to.be.NaN;
 		});
 	});
