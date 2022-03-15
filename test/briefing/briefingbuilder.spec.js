7bcdc76e711a8a7d76b66e28b8a55507ae93eaeb
chris <tigerman33@gmx.net>
2022-03-15 12:57:59 +0100

    Added test that each topic builder should receive its own copy of the battle

--- test/briefing/briefingbuilder.spec.js
+++ test/briefing/briefingbuilder.spec.js
@@ -2,6 +2,7 @@ import { BriefingBuilder } from '../../src/briefing/briefingbuilder.js';
 import { GameObjectFactory } from '../../src/model/gameobjectfactory.js';
 import { Agenda } from '../../src/briefing/agenda.js';
 import { Battle } from '../../src/model/battle.js';
+import { Ship } from '../../src/model/ship.js';
 import sinon from 'sinon';
 import { readFileSync } from 'fs';
 import { valid as isHtml } from 'node-html-parser';
@@ -50,13 +51,43 @@ describe('BriefingBuilder', function() {
 			// No need to do buildTopic.restore() because it's an isolated stub, not an object method
 		});
 
+		it('should pass separate copies of the battle to each topic builder', async function() {
+			const buildTopic1 = sinon.stub().resolves({ html: '' });
+			const buildTopic2 = sinon.stub().resolves({ html: '' });
+			builder.getTopicBuilder.onFirstCall().resolves({ default: buildTopic1 });
+			builder.getTopicBuilder.onSecondCall().resolves({ default: buildTopic2 });
+
+			agenda.topics = { topic1: {}, topic2: {}};
+			battle.getVehicles().forEach(vehicle => vehicle.ship = new Ship({ShipUpgradeInfo:{}}));
+
+			// Expect both topic builders to have been called:
+			await builder.build(battle, agenda);
+			expect(buildTopic1).to.have.been.called;
+			expect(buildTopic2).to.have.been.called;
+
+			// Expect the "battle" argument of each topic builder to have been deeply equal, but not strictly equal:
+			const battle1 = buildTopic1.firstCall.firstArg;
+			const battle2 = buildTopic2.firstCall.firstArg;
+			expect(battle1).to.not.equal(battle2);
+			expect(battle1).to.deep.equal(battle2);
+
+			// Check that all ships are copies, too:
+			const vehicles1 = battle1.getVehicles();
+			const vehicles2 = battle2.getVehicles();
+			expect(vehicles1).to.have.lengthOf(vehicles2.length);
+			for (let i = 0; i < vehicles1.length; i++) {
+				expect(vehicles1[i].ship).to.not.equal(vehicles2[i].ship);
+				expect(vehicles1[i].ship).to.deep.equal(vehicles2[i].ship);
+			}
+		});
+
 		it('should pass along options to the topic builder', async function() {
 			const buildTopic = sinon.stub().returns({});
 			builder.getTopicBuilder.resolves({ default: buildTopic });
 			agenda.topics.testtopic = { option: 'option' };
 			let expected = agenda.topics.testtopic;
 			await builder.build(battle, agenda);
-			expect(buildTopic).to.have.been.calledWith(battle, gameObjectFactory, expected);
+			expect(buildTopic).to.have.been.calledWith(sinon.match.any, gameObjectFactory, expected);
 			// No need to do buildTopic.restore() because it's an isolated stub, not an object method			
 		});
 
