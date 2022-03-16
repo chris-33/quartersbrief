e62e1db86b156a925e6e8313b570433aa1d200b5
chris <tigerman33@gmx.net>
2022-03-16 09:51:58 +0100

    Added .freshCopy()

--- src/model/gameobject.js
+++ src/model/gameobject.js
@@ -59,6 +59,46 @@ class GameObject {
 		this._data.clear();
 	}
 
+	/**
+	 * Returns a fresh copy of this `GameObject`.  
+	 *
+	 * The purpose of this method is to allow getting a fresh instance of an already present `GameObject` 
+	 * without needing go through the (relatively expensive) process of having to retrieve it from a `GameObjectFactory`.
+	 *
+	 * `freshCopy` performs a deep copy of this `GameObject`, where
+	 * - any primitive properties of the data are copied to the destination,
+	 * - any object properties that are themselves `GameObject`s are fresh-copied,
+	 * - any object properties that are _not_ `GameObject`s are copied,
+	 * - lazily-expanding references are not forced to expand.
+	 * 
+	 * @return {GameObject} A new object that has the same class as this one, and whose
+	 * data is a _copy_ of the data of this one.
+	 */
+	freshCopy() {
+		function _copy(data) {
+			let dest;
+			if (Array.isArray(data))
+				dest = [];
+			else 
+				dest = Object.create(Object.getPrototypeOf(data));
+
+			let descs = Object.getOwnPropertyDescriptors(data);
+			for (let prop in descs) {
+				let desc = descs[prop];
+				if (typeof desc.value === 'object' && desc.value !== null)
+					if (desc.value instanceof GameObject)
+						desc.value = desc.value.freshCopy()
+					else
+						desc.value = _copy(desc.value);
+			}
+			Object.defineProperties(dest, descs);
+			return dest;
+
+		}
+
+		return new this.constructor(_copy(this._data));
+	}
+
 	getID() { return this._data.id;	}
 	getName() { return this._data.name; }
 	getNation() { return this._data.typeinfo.nation; }
--- test/model/gameobject.spec.js
+++ test/model/gameobject.spec.js
@@ -1,16 +1,92 @@
 import { GameObject } from '../../src/model/gameobject.js';
+import clone from 'clone';
+import sinon from 'sinon';
 
 describe('GameObject', function() {
 	const TEST_DATA = { 
 		prop1: 'string', 
 		prop2: 0,
 		nested: { prop3: 'prop3' },
-		arr: [ 'prop4' ] 
+		arr: [ 'prop4' ],
+		go: {
+			prop: 'gameobject',
+			typeinfo: {
+				type: "Type1",
+				species: null,
+				nation: "Common"
+			}
+		},
+		inner: {
+			go: {
+				prop: 'gameobject',
+				typeinfo: {
+					type: "Type1",
+					species: null,
+					nation: "Common"
+				}
+			}
+		},
+		typeinfo: {
+			type: "Type1",
+			species: null,
+			nation: "Common"
+		}
 	};
 
 	it('should copy all properties from the source', function() {
 		expect(new GameObject(TEST_DATA)).to
 			.have.property('_data')
 			.that.deep.equals(TEST_DATA);
+	});
+
+	describe('.freshCopy', function() {
+		let gameObject;
+
+		beforeEach(function() {
+			const data = clone(TEST_DATA);
+			data.inner.go = new GameObject(data.inner.go);
+			data.go = new GameObject(data.go);
+
+			gameObject = new GameObject(data);
+		});
+
+		it('should make a deep copy of the source', function() {
+			const other = gameObject.freshCopy();
+			expect(gameObject).to.not.equal(other);
+			expect(gameObject).to.deep.equal(other);			
+		});
+
+		it('should not invoke getters', function() {
+			const spy = sinon.spy();
+			// Invoking getters would force lazily expanded references to expand
+			Object.defineProperty(gameObject._data, 'reference', {
+				get: spy,
+				enumerable: true,
+				configurable: true
+			});
+			gameObject.freshCopy();
+			expect(spy).to.not.have.been.called;
+		});
+
+		it('should make copies of nested properties that are not game objects', function() {
+			const data = gameObject._data;
+			const other = gameObject.freshCopy();
+			for (let key in data)
+				if (typeof data[key] === 'object' && data[key] !== null && !(data[key] instanceof GameObject)) {
+					expect(data[key]).to.not.equal(other._data[key]);
+					expect(data[key]).to.deep.equal(other._data[key]);
+				}
+		});
+
+		it('should contain fresh copies of all nested game objects', function() {
+			const other = gameObject.freshCopy();
+			expect(gameObject._data.go).to.exist.and.be.an.instanceof(GameObject);
+			expect(gameObject._data.go).to.not.equal(other._data.go);
+			expect(gameObject._data.go).to.deep.equal(other._data.go);
+
+			expect(gameObject._data.inner.go).to.exist.and.be.an.instanceof(GameObject);
+			expect(gameObject._data.inner.go).to.not.equal(other._data.inner.go);
+			expect(gameObject._data.inner.go).to.deep.equal(other._data.inner.go);
+		});
 	});
 });
