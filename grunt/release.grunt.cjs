4e10d432a72038ac5b294ca5d120d55d74821b24
chris <tigerman33@gmx.net>
2022-04-04 12:31:30 +0200

    Corrected tag name and message to include leading 'v'

--- grunt/release.grunt.cjs
+++ grunt/release.grunt.cjs
@@ -1,6 +1,8 @@
 module.exports = {
 	options: {
 		npm: false,
+		tagName: 'v<%= version %>',
+		tagMessage: 'v<%= version %>',
 		github: {
 			repo: 'BadIdeaException/quartersbrief',
 			accessTokenVar: 'GITHUB_ACCESS_TOKEN'
