4fd8dfd0f6e7528b50edbd763aac2f291369e41d
chris <tigerman33@gmx.net>
2022-04-09 21:58:09 +0200

    Silenced command output and added install messages instead

--- scripts/postinstall.ps1
+++ scripts/postinstall.ps1
@@ -1,13 +1,19 @@
 $basepath = Join-Path -Path $PSScriptRoot -ChildPath ..
 
-if (!(Test-Path "$basepath\tools\wowsunpack\wowsunpack.exe")) {
-    New-Item -Path "$basepath\tools\wowsunpack" -ItemType "directory"
-    Invoke-WebRequest "https://dl-wows-gc.wargaming.net/projects/mods/wowsunpack.exe" -outfile "$basepath\tools\wowsunpack\wowsunpack.exe"
+if (Test-Path "$basepath\tools\wowsunpack\wowsunpack.exe") {
+    echo "wowsunpack.exe is already installed"
+} else {
+    echo "Installing wowsunpack.exe..."
+    New-Item -Path "$basepath\tools\wowsunpack" -ItemType "directory" -ErrorAction Ignore | Out-Null
+    Invoke-WebRequest "https://dl-wows-gc.wargaming.net/projects/mods/wowsunpack.exe" -outfile "$basepath\tools\wowsunpack\wowsunpack.exe" | Out-Null
 }
 
-if (!(Test-Path "$basepath\tools\gameparams2json\OneFileToRuleThemAll.py")) {
-    New-Item -Path "$basepath\tools\gameparams2json" -ItemType "directory"
-    Invoke-WebRequest "https://github.com/EdibleBug/WoWS-GameParams/tarball/master" -outfile "$basepath\tools\gameparams2json\gameparams2json.tar"
-    tar -xf "$basepath\tools\gameparams2json\gameparams2json.tar" --strip-components=1 --directory="$basepath\tools\gameparams2json\"
-    Remove-Item -Path "$basepath\tools\gameparams2json\gameparams2json.tar"
+if (Test-Path "$basepath\tools\gameparams2json\OneFileToRuleThemAll.py") {
+    echo "Tool to convert GameParams.data to GameParams.json is already installed."
+} else {
+    echo "Installing tool to convert GameParams.data to GameParams.json..."
+    New-Item -Path "$basepath\tools\gameparams2json" -ItemType "directory" -ErrorAction Ignore | Out-Null
+    Invoke-WebRequest "https://github.com/EdibleBug/WoWS-GameParams/tarball/master" -outfile "$basepath\tools\gameparams2json\gameparams2json.tar" | Out-Null
+    tar -xf "$basepath\tools\gameparams2json\gameparams2json.tar" --strip-components=1 --directory="$basepath\tools\gameparams2json" | Out-Null
+    Remove-Item -Path "$basepath\tools\gameparams2json\gameparams2json.tar" | Out-Null
 }
--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -4,10 +4,16 @@ basepath=$(realpath -s "$(dirname ${BASH_SOURCE[0]:-$0})/..")
 
 # Install wowsunpack.exe if it doesn't already exist
 if [[ ! -e "$basepath/tools/wowsunpack/wowsunpack.exe" ]]; then
-	mkdir -p "$basepath/tools/wowsunpack" && curl -o "$basepath/tools/wowsunpack/wowsunpack.exe" https://dl-wows-gc.wargaming.net/projects/mods/wowsunpack.exe
+	echo "Installing wowsunpack.exe..."
+	mkdir --parents "$basepath/tools/wowsunpack" && curl --silent --output "$basepath/tools/wowsunpack/wowsunpack.exe" https://dl-wows-gc.wargaming.net/projects/mods/wowsunpack.exe
+else
+	echo "wowsunpack.exe is already installed."
 fi
 
 # Install tool to convert GameParams.data to GameParams.json
 if [[ ! -e "$basepath/tools/gameparams2json/OneFileToRuleThemAll.py" ]]; then
-	mkdir -p "$basepath/tools/gameparams2json" && curl -L https://github.com/EdibleBug/WoWS-GameParams/tarball/master | tar xz -C "$basepath/tools/gameparams2json/" --strip-components=1
+	echo "Installing tool to convert GameParams.data to GameParams.json..."
+	mkdir --parents "$basepath/tools/gameparams2json" && curl --silent --location https://github.com/EdibleBug/WoWS-GameParams/tarball/master | tar --extract --gunzip --directory="$basepath/tools/gameparams2json/" --strip-components=1
+else
+	echo "Tool to convert GameParams.data to GameParams.json is already installed."
 fi
