474a2ebeb84238a80801bb7063fa22930fbc7613
chris <tigerman33@gmx.net>
2022-04-21 21:46:01 +0200

    Moved agenda scoring completely into SpecificityStrategy

--- test/briefing/agenda.spec.js
+++ test/briefing/agenda.spec.js
@@ -4,55 +4,6 @@ import { Ship } from '../../src/model/ship.js';
 import sinon from 'sinon';
 
 describe('Agenda', function() {
-	describe('.score', function() {
-		it('should calculate scores according to ships, classes, nations and tiers', function() {
-			expect(new Agenda(null, null).score).to.equal(undefined);
-			expect(new Agenda({}, null).score).to.equal(0);
-			expect(new Agenda({ ships: [] }, null).score).to.equal(100);
-			expect(new Agenda({ tiers: [ 8, 9, 10 ], classes: [ 'Cruiser' ]}, null).score).to.equal(20);			
-		});
-	});
-
-	describe('.matches', function() {
-		let battle;
-
-		beforeEach(function() {
-			battle = new Battle();
-			let ship = new Ship({
-				level: 8,
-				name: 'PAAA001_Battleship',
-				typeinfo: { type: 'Ship', nation: 'USA', species: 'Battleship' },
-				ShipUpgradeInfo: {},
-				ShipAbilities: []
-			});
-			sinon.stub(battle, 'getPlayer').returns({ship: ship});
-		});
-
-		it('should throw if the battle does not have a ship for the player', function() {
-			let agenda = new Agenda({}, null);
-			// Expect to throw if getPlayer() does not have a 'ship' property
-			battle.getPlayer.returns({});
-			expect(agenda.matches.bind(agenda, battle)).to.throw();
-			// Expect to throw if getPlayer() does have a 'ship' property but it is not a 'Ship'
-			battle.getPlayer.returns({ ship: {} });
-			expect(agenda.matches.bind(agenda, battle)).to.throw();
-			// Expect to not throw if getPlayer() does have a 'ship' property that is a 'Ship'
-			battle.getPlayer.returns({ ship: sinon.createStubInstance(Ship) });
-			expect(agenda.matches.bind(agenda, battle)).to.not.throw();
-		});
-
-		it('should match a battle only if it fulfills all listed criteria', function() {
-			expect(new Agenda({ ships: [ 'PAAA002_SomethingElse' ] }, null).matches(battle)).to.be.false; // Wrong name
-			expect(new Agenda({ ships: [ 'PAAA001_Battleship' ] }, null).matches(battle)).to.be.true;
-			expect(new Agenda({}, null).matches(battle)).to.be.true;
-			expect(new Agenda({ ships: [ 'PAAA001_Battleship' ], tiers: [ 7 ] }, null).matches(battle)).to.be.false; // Wrong tier
-			expect(new Agenda({ classes: [ 'Battleship', 'Cruiser' ] }, null).matches(battle)).to.be.true;
-			expect(new Agenda({ classes: [ 'Battleship', 'Cruiser' ], tiers: [ 8, 9, 10 ] }, null).matches(battle)).to.be.true;
-			expect(new Agenda({ classes: [ 'Battleship', 'Cruiser' ], tiers: [ 8, 9, 10 ], nations: [ 'Germany' ] }, null).matches(battle)).to.be.false; // Wrong nation
-			expect(new Agenda({ classes: [ 'Battleship', 'Cruiser' ], tiers: [ 8, 9, 10 ], nations: [ 'Germany', 'USA' ] }, null).matches(battle)).to.be.true;
-		});
-	});
-
 	describe('.getTopicNames', function() {
 		it('should return the names of all topics of the agenda', function() {
 			expect(new Agenda(null, {
--- test/briefing/specificitystrategy.spec.js
+++ test/briefing/specificitystrategy.spec.js
@@ -1,41 +1,103 @@
 import { SpecificityStrategy } from '../../src/briefing/specificitystrategy.js';
 import { Agenda } from '../../src/briefing/agenda.js';
 import { Battle } from '../../src/model/battle.js';
+import { Ship } from '../../src/model/ship.js';
+import { GameObjectFactory } from '../../src/model/gameobjectfactory.js';
 import { readFileSync } from 'fs';
 import sinon from 'sinon';
 
 describe('SpecificityStrategy', function() {
 	let TEST_DATA;
-	let strategy;
+	const SHIP_DATA = {
+		name: 'PAAA001_Battleship',
+		class: 'Battleship',
+		tier: 8,
+		nation: 'USA'
+	}
 
+	let strategy;
+	let ship;
 	before(function() {
 		TEST_DATA = JSON.parse(readFileSync('test/briefing/testdata/agendas.json'));
-		strategy = new SpecificityStrategy();
+		ship = Object.create(Ship.prototype);
+		for (let prop in SHIP_DATA) {
+			const getter = `get${prop.charAt(0).toUpperCase()}${prop.substring(1)}`;
+			sinon.stub(ship, getter).returns(SHIP_DATA[prop]);
+		}
+		let gameObjectFactory = new GameObjectFactory();
+		sinon.stub(gameObjectFactory, 'createGameObject').returns(ship);
+		strategy = new SpecificityStrategy(gameObjectFactory);
+	});
+
+	describe('._getScore', function() {
+		it('should return 0 if the agenda has an empty matcher, or no matcher at all', function() {
+			expect(strategy._getScore(null, new Agenda(null, null))).to.equal(0);
+			expect(strategy._getScore(null, new Agenda(null, {}))).to.equal(0);
+		});
+
+		it('should award the correct points for matching ships, classes, nations, and tiers', function() {			
+			const matchers = {
+				ships: [ ship.getName() ],
+				classes: [ ship.getClass() ],
+				tiers: [ ship.getTier() ],
+				nations: [ ship.getNation() ]
+			}
+			for (let prop in matchers)
+				expect(strategy._getScore(ship, new Agenda({ [prop]: matchers[prop] })), prop).to
+					.equal(SpecificityStrategy.POINTS[prop]);
+		});
+
+		it('should return a negative number for not matching a present ships, classes, nations or tiers', function() {
+			[ 'ships', 'classes', 'tiers', 'nations' ].forEach(prop => 
+				expect(strategy._getScore(ship, new Agenda({ [prop]: [ 'not matching ship' ]}))).to
+					.be.below(0));
+		});
 	});
 
 	describe('.chooseAgenda', function() {
+		let battle;
+
+		before(function() {
+			battle = new Battle();
+			sinon.stub(battle, 'getPlayer').returns({});
+		});
+
 		it('should return null if there are no agendas', function() {			
-			expect(strategy.chooseAgenda({}, [])).to.be.null;
+			expect(strategy.chooseAgenda(battle, [])).to.be.null;
 		});
 
 		it('should return null if no agendas fit', function() {
 			let agendas = TEST_DATA.map(agenda => new Agenda(agenda.matches, null));
-			agendas.forEach(agenda => sinon.stub(agenda, 'matches').returns(false));
+			sinon.stub(strategy, '_getScore').returns(-10000);
 			try {
-				expect(strategy.chooseAgenda({}, [])).to.be.null;
+				expect(strategy.chooseAgenda(battle, agendas)).to.be.null;
 			} finally {
-				agendas.forEach(agenda => agenda.matches.restore());
+				strategy._getScore.restore();
 			}
 		});
 
-		it('should return the agenda with the highest score', function() {
+		it('should return an agenda with score 0 if no others fit', function() {
 			let agendas = TEST_DATA.map(agenda => new Agenda(agenda.matches, null));
-			let battle = new Battle();
-			agendas.forEach(agenda => sinon.stub(agenda, 'matches').returns(true));
-			try {				
-				expect(strategy.chooseAgenda(battle, agendas)).to.equal(agendas[3]);
+			sinon.stub(strategy, '_getScore').returns(-10000);
+			strategy._getScore.onFirstCall().returns(0);			
+			try {
+				expect(strategy.chooseAgenda(battle, agendas)).to.equal(agendas[0]);
 			} finally {
-				agendas.forEach(agenda => agenda.matches.restore());
+				strategy._getScore.restore();
+			}			
+		});
+
+		it('should return the agenda with the highest score', function() {
+			let agendas = TEST_DATA.map(agenda => new Agenda(agenda.matches, null))
+
+			// Make each agenda's index its score
+			sinon.stub(strategy, '_getScore');
+			agendas.forEach((agenda, index) => strategy._getScore.withArgs(sinon.match.any, agenda).returns(index));
+			
+			try {				
+				expect(strategy.chooseAgenda(battle, agendas)).to.equal(agendas[agendas.length - 1]);
+			} finally {
+				strategy._getScore.restore();
 			}
 		});
 	});
