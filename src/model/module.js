13264601d2cea1b9df576ee95638469218b8f936
chris <tigerman33@gmx.net>
2022-08-06 14:10:46 +0200

    Made Weapon.mounts enumerable, and based Weapon on includeOwnPropertiesByDefault

--- src/model/module.js
+++ src/model/module.js
@@ -1,5 +1,4 @@
-import DataObject from './dataobject.js';
-import DotNotation from '../util/dotnotation.js';
+import DataObject, { includeOwnPropertiesByDefault } from './dataobject.js';
 
 class Module extends DataObject {
 	constructor(ship, data) {
@@ -18,25 +17,9 @@ class Weapon extends Module {
 		return Object.values(this._data).filter(obj => 
 				typeof obj === 'object' && 'typeinfo' in obj && obj.typeinfo.type === 'Gun');
 	}
-
-	get(key, options) {
-		let path = DotNotation.elements(key);
-		if (path[0] === 'mounts') {
-			// Create a temporary DataObject from this.mounts and hand off the operation to it
-			return new DataObject(this.mounts).get(DotNotation.join(path.slice(1)), options);
-		} else
-			return super.get(key, options);
-	}
-
-	multiply(key, factor) {
-		let path = DotNotation.elements(key);
-		if (path[0] === 'mounts') {
-			// Create a temporary DataObject from this.mounts and hand off the operation to it
-			return new DataObject(this.mounts).multiply(DotNotation.join(path.slice(1)), factor);
-		} else
-			return super.multiply(key, factor);		
-	}
 }
+Object.defineProperty(Weapon.prototype, 'mounts', { enumerable: true });
+includeOwnPropertiesByDefault(Weapon.prototype);
 
 class Artillery extends Weapon {
 	/** Main artillery caliber in mm. */
