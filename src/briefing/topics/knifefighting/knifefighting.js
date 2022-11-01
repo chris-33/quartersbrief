55b38270be03900fb8c0afb3c3a30ae59e8977df
Christian Heger <tigerman33@gmx.net>
2022-11-01 10:40:14 +0100

    Refactored

--- src/briefing/topics/knifefighting/knifefighting.js
+++ src/briefing/topics/knifefighting/knifefighting.js
@@ -28,8 +28,7 @@ function buildHtml(battle, gameObjectFactory, options) {
 		// If options.filter.classes is set, filter the ships list accordingly
 		.filter(ship => options?.filter?.classes?.includes(ship.getClass()) ?? true);
 		
-	const entries = ships
-		.map(ship => {
+	const entries = ships.map(ship => {
 			const entry = { ship, base: {}, max: {} };			
 
 			const guns = ship.get('artillery.mounts.*.numBarrels').reduce((prev, curr) => prev + curr, 0);
@@ -38,30 +37,27 @@ function buildHtml(battle, gameObjectFactory, options) {
 
 			entry.base.dpm = {};
 			ship.get('artillery.mounts.*.ammoList')[0].forEach(ammo => {
-				entry.base.dpm[ammo.get('ammoType').toLowerCase()] = ammo.get('alphaDamage') * guns * 60 / entry.base.reload;
+				let ammoType = ammo.get('ammoType').toLowerCase();
+				if (ammoType === 'cs') ammoType = 'sap';
+				entry.base.dpm[ammoType] = ammo.get('alphaDamage') * guns * 60 / entry.base.reload;
 			});
 			entry.base.dpm.pertinent = Math.max(entry.base.dpm.cs ?? 0, entry.base.dpm.he ?? 0) || entry.base.dpm.ap;
 			entry.base.knifefighting = knifefighting(entry.base);
 
+			shipBuilder.build(ship, HEALTH_BUILD);
+			entry.max.health = ship.getHealth();
+
+			shipBuilder.build(ship, DPM_BUILD);
+			entry.max.reload = ship.get('artillery.mounts.*.shotDelay', { collate: true });
+			entry.max.dpm = {};
+			ship.get('artillery.mounts.*.ammoList')[0].forEach(ammo => {
+				entry.max.dpm[ammo.get('ammoType').toLowerCase()] = ammo.get('alphaDamage') * guns * 60 / entry.max.reload;
+			});
+			entry.max.dpm.pertinent = Math.max(entry.max.dpm.cs ?? 0, entry.max.dpm.he ?? 0) || entry.max.dpm.ap;
+			entry.max.knifefighting = knifefighting(entry.max);
+
 			return entry;
 		});
-	
-	entries.forEach(entry => {
-		shipBuilder.build(entry.ship, HEALTH_BUILD);
-		shipBuilder.build(entry.ship, DPM_BUILD);
-
-		const guns = entry.ship.get('artillery.mounts.*.numBarrels').reduce((prev, curr) => prev + curr, 0);
-		entry.max.health = entry.ship.getHealth();
-		entry.max.reload = entry.ship.get('artillery.mounts.*.shotDelay', { collate: true });
-		
-
-		entry.max.dpm = {};
-		entry.ship.get('artillery.mounts.*.ammoList')[0].forEach(ammo => {
-			entry.max.dpm[ammo.get('ammoType').toLowerCase()] = ammo.get('alphaDamage') * guns * 60 / entry.max.reload;
-		});
-		entry.max.dpm.pertinent = Math.max(entry.max.dpm.cs ?? 0, entry.max.dpm.he ?? 0) || entry.max.dpm.ap;
-		entry.max.knifefighting = knifefighting(entry.max);
-	});
 
 	entries.sort((e1, e2) => e1.max.knifefighting - e2.max.knifefighting);
 
