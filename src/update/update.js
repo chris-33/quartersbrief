33e98ed634a73e3bf88ee281807f6a61b26a8c02
Christian Heger <tigerman33@gmx.net>
2022-11-04 09:40:13 +0100

    Added function each()

--- src/update/update.js
+++ src/update/update.js
@@ -1,3 +1,9 @@
+export function each(step) {
+	return async function(result) {
+		return result.map(async r => await step(r));
+	}
+}
+
 export async function executeSteps(steps) {
 	let result;
 	for (let step of steps) {
--- test/update/update.spec.js
+++ test/update/update.spec.js
@@ -1,7 +1,7 @@
-import { update } from '../../src/update/update.js';
+import { executeSteps, each } from '../../src/update/update.js';
 import sinon from 'sinon';
 
-describe('update', function() {
+describe('executeSteps', function() {
 	it('should call each step in turn with the results of the previous step', async function() {
 		return Promise.all([ 'synchronous', 'asynchronous' ].map(async mode => {
 
@@ -9,11 +9,25 @@ describe('update', function() {
 			const steps = [ 0, 1, 2 ].map(i => sinon.stub()[returnMethod](i));
 			const expected = [ undefined, 0, 1, 2 ];
 
-			let result = await update(steps);
+			let result = await executeSteps(steps);
 			
 			steps.forEach((step, index) => expect(step, `${index} ${mode}`).to.have.been.calledWith(expected[index]));
 			expect(result, `result ${mode}`).to.equal(expected.at(-1));
 
 		}));
+	});
+
+	it('should handle errors in some mysterious and yet unspecified way');
+});
+
+describe('each', function() {
+	it('should call the step function with each array item instead of the array', async function() {
+		const step = sinon.stub();
+		const arg = [ 1, 2, 3 ];
+		await executeSteps([
+			() => arg,
+			each(step)
+		]);
+		arg.forEach(arg => expect(step).to.have.been.calledWith(arg));
 	});
 });
