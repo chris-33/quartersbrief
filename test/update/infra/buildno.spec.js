e97edfc00ea3352f73a7887efdecc4af854fde47
Christian Heger <tigerman33@gmx.net>
2022-11-05 08:41:43 +0100

    Made wows directory a parameter instead of reading from config

--- test/update/infra/buildno.spec.js
+++ test/update/infra/buildno.spec.js
@@ -1,21 +1,8 @@
-import esmock from 'esmock';
+import { latestBuild } from '../../../src/update/infra/buildno.js';
 import mockfs from 'mock-fs';
 
 describe('latestBuild', function() {
-	let latestBuild;
-
-	const config = {
-		wowsdir: '/wows'
-	};
-
-	beforeEach(async function() {
-		this.timeout(3000); // Loading of the modules sometimes takes a while, so increase the timeout to 3s
-
-		// Do a dynamic import so we can supply a mock config.js
-		({ latestBuild } = (await esmock('../../src/update/infra/buildno.js', {}, {
-			'../../src/init/config.js': { default: config },
-		})));
-	});
+	const wowsdir = '/wows'
 
 	afterEach(function() {
 		mockfs.restore();
@@ -26,25 +13,25 @@ describe('latestBuild', function() {
 		const expected = Math.max(...builds);
 		const bin = {};
 		builds
-			.map(buildno => ({ [`${config.wowsdir}/bin/${buildno}`]: {} }))
+			.map(buildno => ({ [`${wowsdir}/bin/${buildno}`]: {} }))
 			.forEach(buildDir => Object.assign(bin, buildDir));
 		mockfs(bin);
-		return expect(latestBuild()).to.eventually.equal(expected);
+		return expect(latestBuild(wowsdir)).to.eventually.equal(expected);
 	});
 
 	it('should ignore files and subfolders not consisting only of digits', function() {
 		const buildno = 1;
 		const bin = {
-			[`${config.wowsdir}/bin/${buildno}`]: {},
-			[`${config.wowsdir}/bin/alphanumeric dir`]: {},
-			[`${config.wowsdir}/bin/alphanumeric file`]: '',
-			[`${config.wowsdir}/bin/${2 * buildno}`]: 'numeric file' // * 2 to make sure this is the highest
+			[`${wowsdir}/bin/${buildno}`]: {},
+			[`${wowsdir}/bin/alphanumeric dir`]: {},
+			[`${wowsdir}/bin/alphanumeric file`]: '',
+			[`${wowsdir}/bin/${2 * buildno}`]: 'numeric file' // * 2 to make sure this is the highest
 		}
 		mockfs(bin);
-		return expect(latestBuild()).to.eventually.equal(buildno);
+		return expect(latestBuild(wowsdir)).to.eventually.equal(buildno);
 	});
 
 	it('should throw if it can\'t read the bin folder', function() {
-		return expect(latestBuild()).to.be.rejected;
+		return expect(latestBuild(wowsdir)).to.be.rejected;
 	});
 });
