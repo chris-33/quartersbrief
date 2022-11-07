6d730f9558aafda7fb9951b919d4751860a64f84
Christian Heger <tigerman33@gmx.net>
2022-11-07 13:20:51 +0100

    Added missing return statement

--- src/init/update.js
+++ src/init/update.js
@@ -3,9 +3,9 @@ import Updater from '../update/updater.js';
 import updateLabels from '../update/update-labels.js';
 import updateGameParams from '../update/update-gameparams.js';
 
-export default async function update() {
+export default function update() {
 	const updater = new Updater(config.wowsdir, paths.data);
 	updater.register(updateLabels)
 	updater.register(updateGameParams);
-	updater.update(config);
+	return updater.update(config);
 }
