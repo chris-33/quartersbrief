2b000de9f725a62fad0db18ff54449d92029a152
Christian Heger <tigerman33@gmx.net>
2022-11-09 07:39:41 +0100

    Externalized to own file

---
+++ src/core/battledatareader.js
@@ -0,0 +1,44 @@
+import { readFile } from 'fs/promises';
+import path from 'path';
+import rootlog from 'loglevel';
+
+/**
+ * Class to read 'tempArenaInfo.json' from the World of Warship replays directory and parse it to an object.
+ * It can recover from `ENOENT` errors (meaning the file or the directory isn't there) and `EACCES` errors (meaning
+ * the file or the replays directory have insufficient permissions).
+ */
+export default class BattleDataReader {
+	constructor(replaydir) {
+		if (!replaydir) 
+			throw new TypeError(`Cannot construct a ${this.constructor.name} without a replaydir`);
+		this.replaydir = replaydir;
+	}
+
+	/**
+	 * Attempt to read `tempArenaInfo.json` and return its contents.
+	 * @return {Object} The contents of `tempArenaInfo.json`, or `null` if the file doesn't exist, the watched directory
+	 * doesn't exist, or either one has insufficient permissions for reading.
+	 */
+	async read() {
+		try {
+			return JSON.parse(await readFile(path.join(this.replaydir, 'tempArenaInfo.json')));
+		} catch (err) {
+			switch (err.code) {
+				case 'ENOENT':
+					// This means either there is currently no battle, or the provided path was wrong
+					// The most likely cause is there is no battle, so this is not entirely unexpected behavior.
+					// We will return null and let the main algorithm figure out where to go from here.
+					// But in case the path was wrong, also put a line in the rootlog
+					rootlog.debug(`No battle found when reading ${this.replaydir}`);
+					return null;
+				case 'EACCES': 
+					// Permission denied. This is not recoverable for quartersbrief, put an error in the rootlog
+					rootlog.error(`Permission denied when trying to read from ${this.replaydir}`);
+					return null;
+				default:
+					throw err;
+			}
+		}		
+		
+	}
+}
---
+++ test/core/battledatareader.spec.js
@@ -0,0 +1,42 @@
+import mockfs from 'mock-fs';
+import BattleDataReader from '../../src/core/battledatareader.js';
+
+describe('.BattleDataReader', function() {
+	const replaydir = '/replays';
+
+	let battleDataReader;
+	
+	beforeEach(function() {
+		battleDataReader = new BattleDataReader(replaydir);
+	});
+
+	afterEach(function() {
+		mockfs.restore();
+	});
+
+	it('should throw if constructed without a replaydir', function() {
+		expect(() => new BattleDataReader()).to.throw();
+	});
+
+	it('should read the file contents', function() {
+		const battleData = {
+			n: 5
+		}
+		mockfs({ [replaydir]: { 'tempArenaInfo.json': JSON.stringify(battleData) }});
+		return expect(battleDataReader.read()).to.eventually.deep.equal(battleData);
+	});
+
+	it('should return null if there is no tempArenaInfo', function() {
+		mockfs({ [replaydir]: {} });
+		return expect(battleDataReader.read()).to.eventually.be.null;
+	});
+
+	it('should recover from missing permissions and return null', function() {
+		mockfs({ [replaydir]: {
+			'tempArenaInfo.json': mockfs.file({
+				mode: 0 // No one has any permissions whatsoever
+			})
+		}});
+		return expect(battleDataReader.read()).to.eventually.be.null;
+	});
+});
