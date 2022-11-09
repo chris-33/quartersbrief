7359569d125d001fa5433e300552d107815e6c95
Christian Heger <tigerman33@gmx.net>
2022-11-09 13:24:00 +0100

    Changed from async/await to event emission

--- test/briefing/briefingbuilder.spec.js
+++ test/briefing/briefingbuilder.spec.js
@@ -1,18 +1,28 @@
-import { BriefingBuilder } from '../../src/briefing/briefingbuilder.js';
+import BriefingBuilder from '../../src/briefing/briefingbuilder.js';
 import { GameObjectFactory } from '../../src/model/gameobjectfactory.js';
 import { Agenda } from '../../src/briefing/agenda.js';
 import { Battle } from '../../src/model/battle.js';
 import sinon from 'sinon';
 import { readFileSync } from 'fs';
+import pug from 'pug';
+import sass from 'sass';
 import { valid as isHtml } from 'node-html-parser';
 import { validate } from 'csstree-validator';
 const isCss = (s) => typeof s === 'string' && validate(s).length === 0;
+
+
+// Helper function that returns a promise that resolves when the emitter emits the specified event.
+function waitFor(emitter, evt) {
+	return new Promise(resolve => emitter.on(evt, (...args) => resolve(args)));
+}
 
 describe('BriefingBuilder', function() {
 	let builder;
 	let gameObjectFactory;
 	let battle;
 	let agenda;
+
+	let buildTopic;
 
 	before(function() {
 		gameObjectFactory = new GameObjectFactory({});
@@ -23,19 +33,27 @@ describe('BriefingBuilder', function() {
 
 	beforeEach(function() {
 		builder = new BriefingBuilder(gameObjectFactory);
-		sinon.stub(builder, 'getTopicBuilder');
-	});
-
-	afterEach(function() {
-		builder.getTopicBuilder.restore();
+		buildTopic = sinon.stub().resolves({})
+		sinon.stub(builder, 'getTopicBuilder').resolves({ default: buildTopic });
 	});
 
 	describe('.build', function() {
+		it('should emit EVT_BRIEFING_START at the beginning', function() {
+			const briefing = builder.build(battle, agenda);
+			return expect(briefing).to.emit(BriefingBuilder.EVT_BRIEFING_START);
+		});
+
+		it('should emit EVT_BRIEFING_FINISH at the end', function() {
+			const briefing = builder.build(battle, agenda);
+			return expect(briefing).to.emit(BriefingBuilder.EVT_BRIEFING_FINISH);
+		});
+
 		it('should build an error message if the import cannot be found', async function() {
 			builder.getTopicBuilder.rejects();
 			sinon.spy(builder, 'buildErrorTopic');
 			try {
-				await builder.build(battle, agenda);
+				const briefing = builder.build(battle, agenda);
+				await waitFor(briefing, BriefingBuilder.EVT_BRIEFING_FINISH);
 				expect(builder.buildErrorTopic).to.have.been.called;				
 			} finally {
 				builder.buildErrorTopic.restore();
@@ -43,21 +61,19 @@ describe('BriefingBuilder', function() {
 		});
 
 		it('should call the buildTopic method for a given topic', async function() {
-			const buildTopic = sinon.stub().returns({});
-			builder.getTopicBuilder.resolves({ default: buildTopic });
-			await builder.build(battle, agenda);
+			await waitFor(builder.build(battle, agenda), BriefingBuilder.EVT_BRIEFING_FINISH);
+			
 			expect(buildTopic).to.have.been.called;
-			// No need to do buildTopic.restore() because it's an isolated stub, not an object method
 		});
 
 		it('should pass along options to the topic builder', async function() {
-			const buildTopic = sinon.stub().returns({});
-			builder.getTopicBuilder.resolves({ default: buildTopic });
 			agenda.topics.testtopic = { option: 'option' };
+			
 			let expected = agenda.topics.testtopic;
-			await builder.build(battle, agenda);
+			
+			await waitFor(builder.build(battle, agenda), BriefingBuilder.EVT_BRIEFING_FINISH);
+
 			expect(buildTopic).to.have.been.calledWith(sinon.match.any, gameObjectFactory, expected);
-			// No need to do buildTopic.restore() because it's an isolated stub, not an object method			
 		});
 
 		it('should pass separate copies of the battle to each topic builder', async function() {
@@ -69,7 +85,8 @@ describe('BriefingBuilder', function() {
 			agenda.topics = { topic1: {}, topic2: {}};
 
 			// Expect both topic builders to have been called:
-			await builder.build(battle, agenda);
+			await waitFor(builder.build(battle, agenda), BriefingBuilder.EVT_BRIEFING_FINISH);
+
 			expect(buildTopic1).to.have.been.called;
 			expect(buildTopic2).to.have.been.called;
 
@@ -80,23 +97,35 @@ describe('BriefingBuilder', function() {
 			expect(battle1).to.deep.equal(battle2);
 		});
 
-		it('should return a promise that resolves to a briefing object with valid HTML', async function() {
-			const buildTopic = sinon.stub().resolves({ html: '<p>topic</p>'});
-			builder.getTopicBuilder.resolves({ default: buildTopic });
-			return expect(builder.build(battle, agenda)).to.eventually
-				.have.property('html')
-				.that.eventually.satisfies(isHtml);
-			// No need to do buildTopic.restore() because it's an isolated stub, not an object method
-		});
+		it('should emit EVT_BRIEFING_TOPIC when a topic has finished rendering', async function() {
+			const topic = { 
+				html: '<p>topic</p>', 
+				css: 'p { color: red }', 
+				caption: 'Topic' 
+			}
+			buildTopic.returns(topic);
 
-		it('should return a promise that resolves to a briefing object with valid CSS', async function() {
-			const buildTopic = sinon.stub().resolves({ scss: 'p { color: red }'});
-			builder.getTopicBuilder.resolves({ default: buildTopic });
-			return expect(builder.build(battle, agenda)).to.eventually
-				.have.property('css')
-				.that.eventually.satisfies(isCss);
-			// No need to do buildTopic.restore() because it's an isolated stub, not an object method
-		});
+			const expected = {
+				html: pug.renderFile('src/briefing/topic.pug', topic),
+				css: sass.compileString(`#topic-0 {${topic.css}}`).css
+			}
 
+			const briefing = builder.build(battle, agenda);
+
+			// Temporary promise for the waitFor helper function
+			// We can't await this directly, because both waitFor and the .emit assertion will
+			// register event handlers, and both need to be registered when build is run
+			const p = waitFor(briefing, BriefingBuilder.EVT_BRIEFING_TOPIC);
+			await expect(briefing).to.emit(BriefingBuilder.EVT_BRIEFING_TOPIC);
+
+
+			const eventData = await p;
+			expect(eventData).to.be.an('array');
+			expect(eventData[0]).to.equal(0);
+			expect(eventData[1]).to.deep.equal(expected);
+
+			expect(eventData[1].html).to.satisfy(isHtml);
+			expect(eventData[1].css).to.satisfy(isCss);
+		});
 	});
 });
