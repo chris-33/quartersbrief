530060266eab70189bde1ee1d05fc6c21cf8f432
Christian Heger <tigerman33@gmx.net>
2022-11-09 07:55:11 +0100

    Renamed SpecificityStrategy to SpecificityChooser

--- test/core/briefingmaker.integration.spec.js
+++ test/core/briefingmaker.integration.spec.js
@@ -5,7 +5,7 @@ import TOML from '@iarna/toml';
 import { BriefingMaker } from '../../src/core/briefingmaker.js';
 import { GameObjectFactory } from '../../src/model/gameobjectfactory.js';
 import { AgendaStore } from '../../src/briefing/agendastore.js';
-import { SpecificityStrategy } from '../../src/briefing/specificitystrategy.js';
+import SpecificityChooser from '../../src/briefing/specificitychooser.js';
 import { valid as isHtml, parse } from 'node-html-parser';
 import { validate } from 'csstree-validator';
 const isCss = (s) => typeof s === 'string' && validate(s).length === 0;
@@ -88,7 +88,7 @@ describe('BriefingMaker @integration', function() {
 	beforeEach(function() {
 		let gameObjectFactory = new GameObjectFactory(MOCK_GAME_DATA);
 		let agendaStore = new AgendaStore(path.join(os.tmpdir(), 'agendas'));
-		let strategy = new SpecificityStrategy(gameObjectFactory);
+		let strategy = new SpecificityChooser(gameObjectFactory);
 		briefingMaker = new BriefingMaker(os.tmpdir(), gameObjectFactory, agendaStore, strategy);
 	});
 
