9e6e4db2aaac60e739948f04a46a9039063c1207
chris <tigerman33@gmx.net>
2022-11-11 13:16:43 +0100

    Changed to a module that exports handler functions

--- res/www/js/io.js
+++ res/www/js/io.js
@@ -1,69 +1,70 @@
-$(document).ready(function() {
-      // Delay socket connection to when the DOM is safe to manipulate
-      // Otherwise, we can have issues where fast-building topics are
-      // delivered before the DOM is ready to receive them
-      // This results in them getting "lost" and never being displayed.
-      const socket = io();
+export const EVT_BRIEFING_START = 'briefingstart';
+export const EVT_BRIEFING_TOPIC = 'briefingtopic';
+export const EVT_BRIEFING_FINISH = 'briefingfinish';
+export const EVT_BATTLE_START = 'battlestart';
+export const EVT_BATTLE_END = 'battleend';
 
-      const briefingStylesheet = new CSSStyleSheet();
-      document.adoptedStyleSheets.push(briefingStylesheet)
+let readyForTopics;
 
-      const EVT_BRIEFING_START = 'briefingstart';
-      const EVT_BRIEFING_TOPIC = 'briefingtopic';
-      const EVT_BRIEFING_FINISH = 'briefingfinish';
-      const EVT_BATTLE_START = 'battlestart';
-      const EVT_BATTLE_END = 'battleend';
+export async function onBriefingStart({ html, css }) {
+	let ready;
+	readyForTopics = new Promise(resolve => ready = resolve);
+	const briefingStylesheet = document.adoptedStyleSheets[0] ?? new CSSStyleSheet();
+	document.adoptedStyleSheets = [ briefingStylesheet ];
+	await briefingStylesheet.replace(css);
+	$('#briefing').html(html);
+	ready();
+}
 
-      socket.on(EVT_BRIEFING_START, async function({ html, css }) {
-           console.log(EVT_BRIEFING_START);
-           document.adoptedStyleSheets = [ briefingStylesheet ];
-           await briefingStylesheet.replace(css);
-           $('#briefing').html(html);
-      });
+export async function onBriefingTopic(index, { html, css }) {
+	const stylesheet = new CSSStyleSheet();
+	await stylesheet.replace(css);
+	document.adoptedStyleSheets.push(stylesheet);
 
-      socket.on(EVT_BRIEFING_TOPIC, async function(index, { html, css }) {
-            console.log(EVT_BRIEFING_TOPIC, index);
-            const stylesheet = new CSSStyleSheet();
-            await stylesheet.replace(css);
-            document.adoptedStyleSheets.push(stylesheet);
+	// Guard against topics getting delivered extremely quickly, i.e. between briefing start and
+	// the completion of its event handler (because onBriefingStart is asynchronous).
+	// This would result in the topic getting "lost", because it would be inserted into the DOM
+	// of the old briefing.
+	await readyForTopics;
 
-            const topic = $(`#topic-${index}`)
-            const transitions = new Set();
-            topic.on('transitionstart', function(event) {
-                  console.log(`Added transition ${event.originalEvent.propertyName} for topic ${index}. Now size ${transitions.size}`)
-                  transitions.add(event.originalEvent.propertyName);
-            });
-            // Helper function that creates a promise which resolves when the transition effect is done.
-            // This allows for a cleaner code style, avoiding deeply nested event handlers ('callback hell')
-            const transition = el => new Promise(resolve => {
-                  // We will just resolve on transitioncancel, because otherwise a cancelled transition would prevent the topic from being shown. 
-                  // Clearly it's better to display it, even if the fancy effect is missing.
-                  function resolveWhenAllDone(event) {
-                        transitions.delete(event.originalEvent.propertyName);
-                        console.log(`Added transition ${event.originalEvent.propertyName} for topic ${index}. Now size ${transitions.size}`)
-                        if (transitions.size === 0)
-                              resolve();
-                  }
-                  el.on('transitionend', resolveWhenAllDone).on('transitioncancel', resolveWhenAllDone)
-            });
-            
+	const topic = $(`#topic-${index}`);
+	// Count how many transitions are started on the topic, so we can wait for all of them to finish.
+	const transitions = new Set();
+	topic.on('transitionstart', function(event) {
+		transitions.add(event.originalEvent.propertyName);
+	});
+	// Helper function that creates a promise which resolves when the transition effect is done.
+	// This allows for a cleaner code style, avoiding deeply nested event handlers ('callback hell')
+	const transition = el => new Promise(resolve => {
+		function resolveWhenAllDone(event) {
+			transitions.delete(event.originalEvent.propertyName);
+			if (transitions.size === 0)
+				resolve();
+		}
+		// We will just resolve on transitioncancel, because otherwise a cancelled transition would prevent the topic from being shown. 
+		// Clearly it's better to display it, even if the fancy effect is missing.
+		el.on('transitionend', resolveWhenAllDone).on('transitioncancel', resolveWhenAllDone)
+	});
+	
+	
+	topic.addClass('loaded');
+	const START_TRANSITION_TIME = 50;
+	// Give the browser x ms to start transitions. If after that time it hasn't started,
+	// we're going to assume it's not going to (e.g. because the element has't actually
+	// being rendered yet).
+	await new Promise(resolve => setTimeout(resolve, START_TRANSITION_TIME));
+	if (transitions.size > 0)
+		await transition(topic);
+	topic.html(html).removeClass('loading');
+	await new Promise(resolve => setTimeout(resolve, START_TRANSITION_TIME));
+	if (transitions.size > 0)
+		await transition(topic);
+	topic.removeClass('loaded');
+}
 
-            topic.addClass('loaded');
-            await transition(topic);
-            topic.html(html).removeClass('loading');
-            await transition(topic);
-            topic.removeClass('loaded');
-      });
-
-      socket.on(EVT_BRIEFING_FINISH, function() {
-            console.log(EVT_BRIEFING_FINISH);
-      });
-
-      socket.on(EVT_BATTLE_START, function() { 
-            console.log(EVT_BATTLE_START);
-      });
-
-      socket.on(EVT_BATTLE_END, function() {
-            console.log(EVT_BATTLE_END)
-      });
-});
+// No-ops. This way we can already register them on the socket instance,
+// so if we ever want to do anything, we just need to change these and not
+// have to remember to register them as event handlers as well. :)
+export async function onBriefingFinish() {}
+export async function onBattleStart() {}
+export async function onBattleEnd() {}
