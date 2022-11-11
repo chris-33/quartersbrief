dabd1dc25a13e4295f1f6535c35f2e16fdb842e9
chris <tigerman33@gmx.net>
2022-11-11 13:17:32 +0100

    Set up socket.io handlers using ES6 modules

---
+++ res/www/js/init.js
@@ -0,0 +1,32 @@
+import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js'
+import * as handlers from './io.js';
+
+// This is now the recommended way of doing $(document).ready()
+$(function() {
+	// Delay socket connection to when the DOM is safe to manipulate
+	// Otherwise, we can have issues where fast-building topics are
+	// delivered before the DOM is ready to receive them
+	// This results in them getting "lost" and never being displayed.
+	const socket = io();
+
+	// Auto-register all event handlers, based on the following convention:
+	// - all events are exported constants whose names are in SCREAMING_SNAKE_CASE
+	// and start with EVT_
+	// - all handlers have the names of their events, sans the EVT_, are in CamelCase
+	// and have the prefix 'on'
+	// E.g. the handler for EVT_SOMETHING is onSomething
+	Object.keys(handlers)
+		.filter(key => key.startsWith('EVT_'))
+		.forEach(key => {
+			let handlerName = key
+				// Lop off EVT_
+				.slice(4)
+				.split('_')
+				// Capitalize the first letter in each word
+				.map(word => word[0].toUpperCase() + word.slice(1).toLowerCase())
+				.join('');
+			handlerName = `on${handlerName}`;
+
+			socket.on(handlers[key], handlers[handlerName]);
+		});
+});
