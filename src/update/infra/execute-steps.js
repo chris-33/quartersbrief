98eb3fc5bed5208b92ce8f9478bb113b5687273d
Christian Heger <tigerman33@gmx.net>
2022-11-18 16:18:34 +0100

    Corrected bug that caused each() to not return a Promise on arrays

--- src/update/infra/execute-steps.js
+++ src/update/infra/execute-steps.js
@@ -2,14 +2,22 @@
  * Returns a function that calls the passed `step` function on each member of an array, or each property of an object, instead of 
  * on the array/object as a whole. The returned function returns the array of step results when called on an array, and an object
  * with the `step` function applied to all properties when called on an object.
+ *
+ * `step` is executed sequentially on each member/property: It is guaranteed that when `step` is run with the second member of the 
+ * array, it has completed (not just started) for the first. (Analagously for objects.)
  * @param  {Function} step The function to apply.
  * @return {Function}      A function that applies `step` on each member of its argument when called with an array, and each property
  * of its argument when called with an object.
  */
 export function each(step) {
 	return async function(arg) {
-		if (Array.isArray(arg)) 
-			return arg.map(async member => await step(member));
+		if (Array.isArray(arg)) {
+			let result = [];
+			for (let member of arg)
+				// Not using Promise.all with arg.map() here because we want the steps to be executed sequentially
+				result.push(await step(member));
+			return result;
+		}
 		else if (typeof arg === 'object') { 
 			let result = {};
 			for (let key in arg)
