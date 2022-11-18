b2d4d019d07866b58c18b2a55546b162cce58a7a
Christian Heger <tigerman33@gmx.net>
2022-11-18 16:26:52 +0100

    Wired armor update into update()

--- src/init/update.js
+++ src/init/update.js
@@ -2,10 +2,12 @@ import config, { paths } from './config.js';
 import Updater from '../update/updater.js';
 import updateLabels from '../update/update-labels.js';
 import updateGameParams from '../update/update-gameparams.js';
+import updateArmor from '../update/update-armor.js';
 
 export default function update() {
 	const updater = new Updater(config.wowsdir, paths.data);
 	updater.register(updateLabels)
 	updater.register(updateGameParams);
+	updater.register(updateArmor);
 	return updater.update(config);
 }
