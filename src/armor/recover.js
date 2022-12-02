8c7fdaaaa05ff48f3a063f9fdbc91310dd2ada1e
chris <tigerman33@gmx.net>
2022-12-02 13:15:38 +0100

    Algorithm complete

--- src/armor/recover.js
+++ src/armor/recover.js
@@ -246,7 +246,7 @@ export function recover(subject, clip, MIN_LENGTH_SQ) {
 				break;
 			case +1:
 				// If we are at the end of a chain, mark the intersection as BOUNCING if the last vertex is
-				// on the same side as the first vertex was, and as CROSSING if it is on the clip side.
+				// on the same side as the first vertex was, and as CROSSING if it is on the other side.
 				curr.crossing = curr.chain - ON_ON === chainStart.chain ? BOUNCING : CROSSING;
 				// Mark chainStart as the same 
 				chainStart.crossing = curr.crossing;
@@ -256,7 +256,7 @@ export function recover(subject, clip, MIN_LENGTH_SQ) {
 	});
 
 	// At this point, all of the intersection vertices of the subject polygon are marked as either
-	// CROSSING or BOUNCING, or have a delayed flag.
+	// CROSSING or BOUNCING, except for those in the middle of a chain.
 	// Copy those labels over to the clip polygon, because the clip polygon crosses the subject at an 
 	// intersection iff the subject crosses the clip.
 	subject.forEach(curr => {
@@ -278,13 +278,17 @@ export function recover(subject, clip, MIN_LENGTH_SQ) {
 	// If no such vertex existed, and every vertex is an ON/ON vertex, 
 	// then subject and clip polygon are identical, and the subject is completely occluded.
 	if (subjectStart === -1 && subject.every(curr => curr.chain === ON_ON))
-		return [];
+		return {
+			subject: [],
+			clip
+		};
 	else [ subjectStart, clipStart ] = [ 
 		{ start: subjectStart, poly: subject },
 		{ start: clipStart, poly: clip }
 	].map(({ start, poly }) => {
 		// Now either start is defined, meaning we already have a valid start vertex, or
 		// start is not defined but not every vertex of the polygon is an ON/ON vertex.
+		// Check which one it is:
 		if (start === -1) {				
 			// At this point, we know there is at least one vertex that is not an ON/ON vertex 
 			// and thus adjacent to an edge that is not a shared edge.
@@ -476,30 +480,78 @@ export function recover(subject, clip, MIN_LENGTH_SQ) {
 		}
 	});
 
-	// Step 4: Split the polygons at fused vertices.
+	// Step 4: Break the polygons up at fused vertices. 
 	// 
-	// Trace the polygon, adding vertices to the current component
-	// When we hit a fuse pair, start a new component and shelve the current one as unfinished
-	// When the current component is complete, add it to the list of result polygons and unshelve the previous unfinished polygon 
-	// A component is complete when we hit the fuse partner of its first vertex, or we have exhausted the input polygon (only for the very first result component)
+	//     +                           +
+	//    / \     +                   / \            +
+	//   /   \   / \     ------->    /   \          / \
+	//  /     \ /   \               /     \        /   \
+	// +-------+-----+             +-------+      +-----+
+	//       fused                component 1   component 2
+	//       
+	// To do this, we trace the polygon vertex for vertex. When we hit a fused vertex, that means one of two things: Either the polygon must be broken
+	// up at this vertex, i.e. we need to start a new result component, or we just finished a result component. We can tell which one it is by checking
+	// the fused vertex's fuse partner: If it is the fused vertex which started the current result component, we just finished this result component. 
+	// Thus, this algorithm works as follows:
+	// 
+	// - Trace the polygon vertex for vertex, adding vertices to the current result component.
+	// - If the current vertex is a fused vertex, check if its fuse partner is the current result component's start. If it is, add the current result
+	//   component to the result. Unshelve the topmost unfinished result component from the stack of unfinished result components and add the fused
+	//   vertex to it.
+	// - If the current vertex is a fused vertex, but its fuse partner is not the current result component's start, we need to start a new result component.
+	//   Shelve the current result component on the stack of unfinished components. Start a new result component, and add the current vertex to it.
 	let [ subjects, clips ] = [ subject, clip ].map(poly => {
+		// A list of components which are the input polygon, broken up at fused vertices
 		const result = [];
+		// A stack of as yet incomplete result components
 		const unfinished = [];
+		// The current result component
 		let current = [];
+		// Trace the polygon vertex for vertex
 		for (let i = 0; i < poly.length; i++) {		
+			// p is the current polygon entry (i.e. vertex + metadata)
 			let p = poly[i];
+			// If p is a fused vertex...
 			if (p.fused) {
-				if (p.fused === current[0]?.fused) {
+				// Check if its fuse partner is the beginning of the current polygon.
+				if (p.fused === current[0]) {
+					// We just finished a result component.
+					// Add the current result component (the one we have now finished) to the overall result.
 					result.push(current);
+					// Pop the topmost unfinished component from the stack to keep working on.
 					current = unfinished.pop();
 				} else {
+					// We have encountered a "new" fused vertex and need to start a new component.
+					// Push the current result component onto the stack as unfinished, and start a new component.
 					unfinished.push(current);
 					current = [];
 				}			
 			}
+			// Add the current vertex to the current component.
 			current.push(p);
 		}
-		result.push(current);		
+		// If the current polygon is not empty, also push it to the result.
+		// (This happens anytime tracing did not start on a fused vertex)
+		if (current.length > 0)
+			result.push(current);
 		return result;
-	})
+	});
+
+	// Step 5: Clean up.
+	// Project back to vertices, then remove collinear vertices, and polygons with less than three vertices.
+	[ subjects, clips ] = [subjects, clips ].map(polys => 
+		polys.map(poly => poly
+			.map(p => p.vertex)
+			// Filter out collinear vertices
+			.filter((vertex, index, poly) => {
+				const prev = poly[(index - 1 + poly.length) % poly.length];
+				const next = poly[(index + 1) % poly.length];
+				return Math.abs(area(prev, vertex, next)) > EPSILON;
+			}))
+		.filter(poly => poly.length >= 3));
+
+	return {
+		subject: subjects,
+		clip: clips
+	}
 }
