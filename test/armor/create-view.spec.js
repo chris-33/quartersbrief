1dd4ce20c313ae07e81e449dc28fdb60fba14af2
Christian Heger <tigerman33@gmx.net>
2023-01-08 15:13:49 +0100

    Added better tests for merge-phase error handling

--- test/armor/create-view.spec.js
+++ test/armor/create-view.spec.js
@@ -42,20 +42,51 @@ describe('createView', function() {
 		expect(result['2'][1]).to.have.deep.members(expected['2'][1]);
 	});	
 
-	it('should recover from zero-length segment errors when assembling result polygons', async function() {
-		const polybool = (await import('polybooljs')).default;
-		sinon.stub(polybool, 'selectUnion')
-			.callThrough()
-			.onFirstCall().throws(new TypeError('Zero-length segment detected'));
-		let createView = (await esmock('../../src/armor/create-view.js', {
-			'polybooljs': polybool
-		})).default;
+	describe('zero-length errors when assembling result polygons', function() {
+		let createView;
+		let polybool;
 
-		return expect(createView(TEST_DATA, 2)).to
+		beforeEach(async function() {
+			polybool = (await import('polybooljs')).default;
+			sinon.stub(polybool, 'selectUnion').callThrough();
+			createView = (await esmock('../../src/armor/create-view.js', {
+				'polybooljs': polybool
+			})).default;
+		});
+
+		afterEach(function() {
+			polybool.selectUnion.restore();
+		});
+
+		it('should realign and retry upon the first zero-length error', async function() {
+			const expected = [ [ 1, 3 ], [ 3, 3 ], [ 3, 1 ], [ 1, 1 ] ];
+			polybool.selectUnion
+				.onFirstCall().throws(new TypeError('Zero-length segment detected'));
+
+			const p = createView(TEST_DATA, 2);
 			// The zero-length error we simulated polybool throwing should be swallowed:
-			.be.fulfilled
-			// The erroring triangle should have been omitted from the result, making the result
-			// a triangle instead of a square:
-			.and.eventually.have.nested.property('1[0]').that.is.an('array').with.lengthOf(3);
+			await expect(p).to.be.fulfilled;
+			
+			const result = await p;
+			expect(result).to.have.nested.property('1[0]').that.is.an('array').with.deep.members(expected);
+			// There should have been a second call to selectUnion, and since the input was not actually misaligned
+			// it should have mirrored the first call
+			expect(polybool.selectUnion.secondCall.args).to.deep.equal(polybool.selectUnion.firstCall.args);
+		});
+
+		it('should ignore the offending triangle upon the second zero-length error', async function() {
+			polybool.selectUnion
+				.callThrough()
+				.onFirstCall().throws(new TypeError('Zero-length segment detected'))
+				.onSecondCall().throws(new TypeError('Zero-length segment detected'));
+
+			return expect(createView(TEST_DATA, 2)).to
+				// The zero-length error we simulated polybool throwing should be swallowed:
+				.be.fulfilled
+				// The erroring triangle should have been omitted from the result, making the result
+				// a triangle instead of a square:
+				.and.eventually.have.nested.property('1[0]').that.is.an('array').with.lengthOf(3);
+		});
+
 	});
 });
