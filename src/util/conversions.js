3a418f0c165a3fa11142f0fe73da09c4016f2ae2
Christian Heger <tigerman33@gmx.net>
2023-01-16 09:47:14 +0100

    Remove unused torpedo damage conversion

--- src/util/conversions.js
+++ src/util/conversions.js
@@ -1,6 +1,5 @@
 const conversions = {
 	BWToMeters: bw => bw * 30,
-	TorpDamageToActual: x => x * 0.36, // @reveng Torpedo damage needs to be multiplied by ~0.36 to get the actual alpha damage, but I can't see where this value comes from
 }
 
 export { conversions };
