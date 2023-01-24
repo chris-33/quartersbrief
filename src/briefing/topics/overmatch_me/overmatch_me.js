ad7a34da5e0d5be5bcea11a5de881e30541425ef
Christian Heger <tigerman33@gmx.net>
2023-01-24 14:09:34 +0100

    Sort ships by caliber

--- src/briefing/topics/overmatch_me/overmatch_me.js
+++ src/briefing/topics/overmatch_me/overmatch_me.js
@@ -2,6 +2,10 @@ import Topic from '../../topic.js';
 import { ShipBuilder } from '../../../util/shipbuilder.js';
 import clone from 'clone';
 
+const TOP_BUILD = {
+	modules: 'top'
+}
+
 export default class OvermatchMeTopic extends Topic {
 	caption = 'Overmatch Threat';
 
@@ -10,10 +14,14 @@ export default class OvermatchMeTopic extends Topic {
 		options.filter = options.filter ?? {};
 		options.filter.teams = [ 'enemies' ];
 
+		const shipBuilder = new ShipBuilder(this.gameObjectFactory);
 		const locals = await super.getPugData(battle, options);
-		locals.ships = locals.ships.filter(ship => 'artillery' in ship);
+		locals.ships = locals.ships
+			.filter(ship => 'artillery' in ship)
+			.map(ship => shipBuilder.build(ship, TOP_BUILD))
+			.sort((ship1, ship2) => ship2.artillery.getCaliber() - ship1.artillery.getCaliber());
 
-		locals.ownship = new ShipBuilder(this.gameObjectFactory).build(locals.teams.player, { modules: 'top' });
+		locals.ownship = shipBuilder.build(locals.teams.player, { modules: 'top' });
 		locals.armor = {
 			side: await this.armorViewer.view(locals.ownship, 'side'),
 		};
