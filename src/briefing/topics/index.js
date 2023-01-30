6f86efe1855aca8d78ac8cc730c9aea05572aed1
chris <tigerman33@gmx.net>
2023-01-30 11:12:59 +0100

    Disable PenYouTopic for the time being

--- src/briefing/topics/index.js
+++ src/briefing/topics/index.js
@@ -4,7 +4,7 @@ export { default as KnifefightingTopic } from './knifefighting/knifefighting.js'
 export { default as OvermatchMeTopic } from './overmatch_me/overmatch_me.js';
 export { default as OvermatchYouTopic } from './overmatch_you/overmatch_you.js';
 export { default as PenMeTopic } from './pen_me/pen_me.js';
-export { default as PenYouTopic } from './pen_you/pen_you.js';
+// export { default as PenYouTopic } from './pen_you/pen_you.js';
 export { default as RadarTopic } from './radar/radar.js';
 export { default as TorpedoesTopic } from './torpedoes/torpedoes.js';
 export { default as WinrateTopic } from './winrate/winrate.js';
