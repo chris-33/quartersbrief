99f75d3b524eeb3bda0edf642d76e9d2eca2a2b5
chris <tigerman33@gmx.net>
2023-03-01 11:58:33 +0100

    Add topic HE & SAP Penetration

---
+++ src/briefing/topics/pen_you/pen_you.js
@@ -0,0 +1,38 @@
+import Topic from '../../topic.js';
+import { ShipBuilder } from '../../../util/shipbuilder.js';
+import { SKILLS } from '../../../model/captain.js';
+import clone from 'clone';
+
+export default class PenYouTopic extends Topic {
+	caption = 'HE & SAP Penetration';
+
+	async getPugData(battle, options) {
+		options = clone(options) ?? {};
+		options.filter = options.filter ?? {};
+		options.filter.teams = [ 'enemies' ];
+
+		const locals = await super.getPugData(battle, options);
+
+		const shipBuilder = new ShipBuilder(this.gameObjectFactory);
+		let ownship = shipBuilder.build(locals.teams.player, { modules: 'top' });
+		
+		locals.ownship = {
+			he: ownship.get('artillery.mounts.*.ammoList', { collate: true }).find(ammo => ammo.get('ammoType') === 'HE')?.get('alphaPiercingHE'),
+			sap: ownship.get('artillery.mounts.*.ammoList', { collate: true }).find(ammo => ammo.get('ammoType') === 'CS')?.get('alphaPiercingCS')
+		}
+		ownship = shipBuilder.build(ownship, { skills: [ SKILLS.INERTIA_FUSE_FOR_HE_SHELLS ] });
+		locals.ownship.ifhe = ownship.get('artillery.mounts.*.ammoList', { collate: true }).find(ammo => ammo.get('ammoType') === 'HE')?.get('alphaPiercingHE');
+		
+		locals.armors = {};
+		for (let ship of locals.ships)
+			locals.armors[ship.getName()] = {};
+
+		for (let view of [ 'side', 'top' ])
+			await Promise.all(locals.ships.map(ship => this.armorViewer
+				.view(ship, view)
+				.catch(err => err) 
+				.then(armor => locals.armors[ship.getName()][view] = armor)));
+
+		return locals;
+	}
+}
