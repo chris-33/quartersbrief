b37abe2fc7cf37b7495e92d5099621d9acc66198
chris <tigerman33@gmx.net>
2023-03-11 15:56:18 +0100

    Remove obsolete todo

--- src/core/agendacontroller.js
+++ src/core/agendacontroller.js
@@ -6,15 +6,6 @@ import rootlog from 'loglevel';
  * choosing one of them for a given `Battle` using a `chooser`.
  */
 export default class AgendaController {
-	// @todo Feature: Agendas can extend each other.
-	// This would require the AgendaController to perform crude linking and compiling tasks:
-	// - LINK the extend source given in the agenda to the matching agenda object from one of the sources
-	// - COMPILE the agenda contents based on the extended agenda and the extending agenda
-	// This has to be done in the AgendaController (as opposed to the AgendaStore), because 
-	// the AgendaController is the only place that has access to all sources. 
-	// If we did it in the AgendaStore, agendas could only extend from others in the same location. 
-	// But we want to have a default set of agendas that can then be altered by the user.
-
 	constructor() {
 		throw new TypeError(`AgendaController cannot be instantiated with new. Use the static create method instead.`);
 	}
