ad74a1336948a4a5d1a1ec70bab67456c916f301
chris <tigerman33@gmx.net>
2023-07-13 20:42:32 +0200

    Fix a bug in the Hydro topic that causes labels to be too far up when there is a lot of space between them.

--- src/briefing/topics/hydro/layout.js
+++ src/briefing/topics/hydro/layout.js
@@ -27,14 +27,21 @@ function layoutRects(rects, bounds, spacing = 0) {
 
 	// Calculate the actual (current) height (including space) that labels are taking up
 	const actual = rects[rects.length - 1].bottom - rects[0].top;
-	// Calculate the sum of their individual heights. 
-	const nominal = rects.reduce((prev, curr) => prev + curr.height, 0);
+	// Calculate the sum of their individual heights. This is the MINIMUM space we will need if we forgo all spacing.
+	let nominal = rects.reduce((prev, curr) => prev + curr.height, 0);
 	// Reduce spacing if there is not enough room to use preferred value
 	spacing = Math.min(spacing, (bounds.height - nominal) / (rects.length - 1));
+	// Adjust nominal to include whatever space we can still add
+	nominal += (rects.length - 1) * spacing;
 
 	// Calculate the difference between the sum of individual heights and the total height
 	// This is how much space we are missing	
-	const overlap = Math.abs(actual - nominal) + (rects.length - 1) * spacing;
+	const overlap = -1 * (actual - nominal);
+
+	// If there is no overlap, we don't need to layout
+	if (overlap <= 0) {
+		return rects.map(rect => ({ x: rect.x, y: rect.y }));
+	}
 
 	// Check how much space is available on the top and bottom with the way things are currently layed out
 	const availTop = rects[0].top - bounds.top;
