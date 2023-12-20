74ca0190b92383b1f10733cb803e28b4c89b8d08
chris <tigerman33@gmx.net>
2023-12-20 14:23:51 +0100

    More readable error message

--- src/update/infra/invarianterror.js
+++ src/update/infra/invarianterror.js
@@ -1,12 +1,12 @@
 /**
  * The exception that will be thrown if an invariant assertion fails.
- * It includes a description of the invariant that failed and a list
- * of counterexamples: the data sets that caused the assertion to fail.
+ * It includes a description of the invariant that failed and a 
+ * counterexample: the data set that caused the assertion to fail.
  * This will make finding the cause of the error a lot easier, since the
  * game's data has grown to be pretty massive.
  */
 export default class InvariantError extends Error {
 	constructor(invariant, counterexample) {		
-		super(`Checking invariant '${invariant}' failed for ${counterexample}`);
+		super(`Checking invariant '${invariant}' failed for ${counterexample.name}`);
 	}
 }
