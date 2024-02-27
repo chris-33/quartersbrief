018a90c0c58314a4d07248403fa03ab7d8539049
chris <tigerman33@gmx.net>
2024-02-27 17:59:41 +0100

    Fixed a bug that caused torpedo launchers to be unable to have more than one torpedo type [[!]]

--- src/model/gun.js
+++ src/model/gun.js
@@ -7,7 +7,7 @@ const AMMO_LIST = compile('ammoList.*');
 export default class Gun extends GameObject {
 	get ammos() {
 		let ammos = {};
-		AMMO_LIST.get(this._data).forEach(ammo => ammos[ammo.ammoType] = ammo);
+		AMMO_LIST.get(this._data).forEach(ammo => ammos[ammo.ammoType ?? ammo.name] = ammo);
 		return ammos;
 	}
 
--- test/model/gun.spec.js
+++ test/model/gun.spec.js
@@ -1,5 +1,6 @@
 import Gun from '../../src/model/gun.js';
 import Shell from '../../src/model/shell.js';
+import Torpedo from '../../src/model/torpedo.js';
 
 describe('Gun', function() {
 	let gun;
@@ -17,11 +18,28 @@ describe('Gun', function() {
 		});
 	});
 
-	it('should have property ammos with all ammos', function() {		
-		expect(gun).to.have.property('ammos').that.is.an('object');
-		[ 'he', 'sap', 'ap' ].forEach(ammoType => {
-			expect(gun.ammos).to.have.property(ammoType).that.is.an.instanceOf(Shell);
-			expect(gun.ammos[ammoType]._data).to.deep.equal(SHELLS.find(shell => shell.ammoType === ammoType));
+	describe('.ammos property', function() {
+		it('should be a hash from ammo type to projectile for all ammos', function() {		
+			expect(gun).to.have.property('ammos').that.is.an('object');
+			[ 'he', 'sap', 'ap' ].forEach(ammoType => {
+				expect(gun.ammos).to.have.property(ammoType).that.is.an.instanceOf(Shell);
+				expect(gun.ammos[ammoType]._data).to.deep.equal(SHELLS.find(shell => shell.ammoType === ammoType));
+			});
+		});
+
+		it('should be a hash from projectile name to projectile for ammos that have no ammo type', function() {		
+			const AMMO_NAMES = [ 'Projectile1', 'Projectile2' ];
+			const TORPEDOES = AMMO_NAMES.map(ammoName => ({
+				alphaDamage: 1000,
+				name: ammoName
+			}));
+			gun = new Gun({ ...GUN, ammoList: TORPEDOES.map(torpedo => new Torpedo(torpedo)) });
+
+			expect(gun).to.have.property('ammos').that.is.an('object');		
+			AMMO_NAMES.forEach(ammoName => {
+				expect(gun.ammos).to.have.property(ammoName).that.is.an.instanceOf(Torpedo);
+				expect(gun.ammos[ammoName]._data).to.deep.equal(TORPEDOES.find(torpedo => torpedo.name === ammoName));
+			});
 		});
 	});
 
